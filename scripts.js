7d5d25dbcee680e188e7ce4384b5ffe93df14efe
Mikko <mkarvonen34@gmail.com>
2020-08-28 08:05:46 +0300

    Show hours and minutes

--- scripts.js
+++ scripts.js
@@ -1,18 +1,29 @@
-let i = 0;
+let date = new Date();
 
-drawClock(i);
+let seconds = date.getSeconds() * 6;
+let minutes = date.getMinutes() * 6;
+let hours = date.getHours() * 30;
+
+drawClock(seconds, "seconds");
+drawClock(minutes, "minutes");
+drawClock(hours, "hours");
 
 setInterval(function () {
-  i += 6;
-  drawClock(i);
+  seconds += 6;
+  if (seconds >= 360) {
+    seconds = 0;
+    minutes += 6;
+    drawClock(minutes, "minutes");
+    if (minutes >= 360) {
+      minutes = 0;
+      hours += 15;
+      drawClock(hours, "hours");
+    }
+  }
+  drawClock(seconds, "seconds");
 }, 1000);
 
-function drawClock(i) {
-  var style = document.getElementById("style");
-  style.innerHTML = `
- #clock{
-  background-image:
-     conic-gradient(from ${i}deg, #fff, #000);
-}`;
-  document.head.appendChild(style);
+function drawClock(i, id) {
+  let element = document.getElementById(id);
+  element.style = `background-image: conic-gradient(from ${i}deg, #fff, #000)`;
 }
